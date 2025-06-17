bbc1fb31f951f65c75b2f2b926d64b1bbcb08b6b
bukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2025-06-17 10:58:35 +0100

    users

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ const {
 const authRoutes = require("./routes/authRoutes.js");
 const cartRoutes = require("./routes/cartRoutes.js");
 const wishlistRoutes = require("./routes/wishlistRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
+
 const mlmRoutes = require("./routes/mlmRoutes.js");
 const withdrawalRequestRoutes = require("./routes/withdrawalRequestRoutes.js");
 
 const paymentRoutes = require("./routes/paymentRoutes.js");
 const commissionRoutes = require("./routes/commissionRoutes.js");
-
+const userRoutes = require("./routes/userRoutes.js");
 const notificationRoutes = require("./routes/notificationRoutes.js");
 const payoutcardRoutes = require("./routes/payoutcardRoutes.js");
 const walletRoutes = require("./routes/walletRoutes.js");
@@ -65,14 +65,17 @@ app.use(
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+app.use("api/users", userRoutes);
 app.use("/api/mlm", mlmRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/user", userRoutes);
+
 app.use("/api/cart", cartRoutes);
 app.use("/api/withdrawal", withdrawalRequestRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/commission", commissionRoutes);
 app.use("/api/payout-card", payoutcardRoutes);
+
 app.use("/api/wallet", walletRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/reviews", reviewRoutes);
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController");
-const authMiddleware = require("../middleware/authMiddleware"); // assuming you have this for req.user
-
+const authMiddleware = require("../middlewares/authMiddleware");
 // Make wallet payment
 router.post("/wallet", authMiddleware, paymentController.walletPayment);
 

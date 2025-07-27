1d030e0d7e9e1dec5de3b0bc959f909a27185ab4
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2025-07-27 05:02:41 +0100

    updated

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const notificationController = require("../controllers/notificationController");
 const authMiddleware = require("../middlewares/authMiddleware");
-
+const adminMiddleware = require("../middlewares/adminMiddleware");
 const router = express.Router();
 
 router.get("/user", authMiddleware, notificationController.getMyNotifications);
 
-// Get admin notifications with optional filters
-router.get("/admin", notificationController.getAdminNotifications);
+router.get(
+  "/admin",
+  adminMiddleware,
+  notificationController.getAdminNotifications
+);
 router.post(
   "/read/:id",
   authMiddleware,

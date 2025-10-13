ef119eec867a13e751799ceada44f9c86b20fa98
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2025-10-13 14:13:28 +0100

    job removed

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ app.use("/api/searched-words", searchedWordRoutes);
 // Start server after DB connects
 connectDB()
   .then(() => {
-    startPaymentFallbackJob();
+    // startPaymentFallbackJob();
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
